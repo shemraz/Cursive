bb8130d56c54c0fb59eada847599c3b290b83cbf
shemraz <shem.raz@outlook.com>
2026-02-09 19:27:03 +0000

    ⚰️ Clean up unused functions

--- src/Cursive.jl
+++ src/Cursive.jl
@@ -6,10 +6,11 @@ using Chain
 
 function ink(input::String; sample::Vector{Int64} = [255,255,255], distance::Float64 = 12.5, filter::Bool = false)
     img = load(input)
+
     if filter == true
-        # img = RGBA.(apply_filter(load(input)))
         apply_filter!(img)
     end
+
     img_rgba = RGBA.(img)
     sample_rgba = @chain (sample ./ 255) RGBA(_..., 1)
     result = map(img_rgba) do px
@@ -20,13 +21,6 @@ function ink(input::String; sample::Vector{Int64} = [255,255,255], distance::Flo
         end
     end
     save("test/output.png", result)
-end
-
-function apply_filter(img::Matrix{ColorTypes.RGB{FixedPointNumbers.N0f8}})::Matrix{ColorTypes.Gray{FixedPointNumbers.N0f8}}
-    @chain img begin
-        @. Gray
-        opening
-    end
 end
 
 function apply_filter!(img::Matrix{ColorTypes.RGB{FixedPointNumbers.N0f8}})::Matrix{ColorTypes.Gray{FixedPointNumbers.N0f8}}
