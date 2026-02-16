0c88b1c37d85f447542d622e3a9ed43b73d6f8df
shemraz <shemraz@tuta.com>
2026-02-16 13:02:47 +0000

    ♻️ Separte CLI from module into a runner script

---
+++ src/CLI.jl
@@ -0,0 +1,28 @@
+using ArgMacros
+
+function main()
+    @inlinearguments begin
+        @argumentdefault Float64 12.5 difference "--difference" "-d"
+        @arghelp "Maximum difference from mean background value to remove."
+
+        @argumentdefault Float64 0.2 segment_threshold "--segment_threshold" "-s"
+        @arghelp "Threshold for image segmentation algorithm (unseeded region growing). Used to determine mean background value."
+
+        @argumentflag colour "--preserve_colour"
+        @arghelp "Skips morphological filtering to preserve colour of ink. Warning: less performant than default."
+
+        @positionalrequired String input "input_file"
+        @arghelp "Path to the input file."
+
+        @positionaldefault String "result.png" output "output_file" 
+        @arghelp "Path to the output file."
+    end
+    img = load(input)
+    result = extract!(img; difference=difference, threshold=segment_threshold, colour=colour)
+    save(output, result)
+    println("Saved ink to $output.")
+end
+
+include("Cursive.jl")
+using .Cursive
+main()
--- src/Cursive.jl
+++ src/Cursive.jl
@@ -1,6 +1,7 @@
 module Cursive
 
-using ImageCore, FileIO, ImageMorphology, ImageSegmentation, FixedPointNumbers, ArgMacros
+using ImageCore, ImageMorphology, ImageSegmentation
+using FileIO, FixedPointNumbers
 
 
 function sample(img::AbstractMatrix{RGB{N0f8}}, threshold::Float64)::RGB{N0f8}
@@ -32,29 +33,4 @@ function apply_filter!(img::AbstractMatrix{RGB{N0f8}})::AbstractMatrix{RGB{N0f8}
     img .= Gray.(img) |> opening
 end
 
-function main()
-    @inlinearguments begin
-        @argumentdefault Float64 12.5 difference "--difference" "-d"
-        @arghelp "Maximum difference from mean background value to remove."
-
-        @argumentdefault Float64 0.2 segment_threshold "--segment_threshold" "-s"
-        @arghelp "Threshold for image segmentation algorithm (unseeded region growing). Used to determine mean background value."
-
-        @argumentflag colour "--preserve_colour"
-        @arghelp "Skips morphological filtering to preserve colour of ink. Warning: less performant than default."
-
-        @positionalrequired String input "input_file"
-        @arghelp "Path to the input file."
-
-        @positionaldefault String "result.png" output "output_file" 
-        @arghelp "Path to the output file."
-    end
-    img = load(input)
-    result = extract!(img; difference=difference, threshold=segment_threshold, colour=colour)
-    save(output, result)
-    println("Saved ink to $output.")
-end
-
 end # module Cursive
-
-Cursive.main()
