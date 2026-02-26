b7bd04df8dd3e4e77e89777c043cfe323519607f
shemraz <shem.raz@outlook.com>
2026-02-26 13:24:06 +0000

    🎨 Move argument parser to separate function; ensure main returns 0

--- src/CLI.jl
+++ src/CLI.jl
@@ -1,7 +1,7 @@
 using ArgMacros
 
-function main(args)
-    @inlinearguments begin
+function parse_args(ARGS)
+    args = @tuplearguments begin
         @argumentdefault Float64 12.5 difference "--difference" "-d"
         @arghelp "Maximum difference from mean background value to remove."
 
@@ -17,9 +17,15 @@ function main(args)
         @positionaldefault String "result.png" output "output_file" 
         @arghelp "Path to the output file."
     end
-    img = load(input)
-    result = extract!(img; difference=difference, threshold=segment_threshold, colour=colour)
-    save(output, result)
-    println("Saved ink to $output.")
+    return args
+end
+
+function main(ARGS)
+    args = parse_args(ARGS)
+    img = load(args.input)
+    result = extract!(img; difference=args.difference, threshold=args.segment_threshold, colour=args.colour)
+    save(args.output, result)
+    println("Saved ink to $(args.output).")
+    return 0
 end
 @main
